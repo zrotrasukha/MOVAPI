ccf1047ffb5a7fdbe9d067378dde8389486458e0
zrotrasukha <zrotrasukha@gmail.com>
2026-03-09 12:59:18 +0000

    Add readIDParams helper

---
+++ cmd/api/helpers.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParams(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, err
+	}
+	return id, nil
+}
+
