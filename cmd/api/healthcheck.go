cacf2468fbbf1922a175e63c85ff52040bcadfe1
zrotrasukha <zrotrasukha@gmail.com>
2026-03-10 00:51:01 +0000

    Improve healthCheckerHandler with writeJSON() hepler

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available ")
-	fmt.Fprintf(w, "environtment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version : %s\n", version)
+	data := map[string]string{
+		"status":  "available",
+		"env":     app.config.env,
+		"version": version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 }
