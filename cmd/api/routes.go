cc9fae6e7112885ce924f190d737f777cf9f51e0
zrotrasukha <zrotrasukha@gmail.com>
2026-03-10 18:45:24 +0000

    Implement createMovieHandler with JSON decoding and error handling; update routes for POST method

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,8 +10,9 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
 	return router
