a3926ad50299216a5084f5436a2fd438b12defd4
zrotrasukha <zrotrasukha@gmail.com>
2026-03-11 17:16:26 +0000

    Add JSON marshaling and unmarshaling for Runtime type with error handling to satisfy Unmarshal.Unmarshaler interface

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -3,7 +3,10 @@ package data
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = fmt.Errorf("invalid runtime format")
 
 type Runtime int32
 
@@ -13,4 +16,25 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
