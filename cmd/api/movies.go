486b3b6b002ec8ddac8642d0da9fc7968bde0de9
zrotrasukha <zrotrasukha@gmail.com>
2026-03-12 18:47:27 +0000

    Add movie validation logic in createMovieHandler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/zrotrasukha/MOVAPI/internal/data"
+	"github.com/zrotrasukha/MOVAPI/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.faildValidationResponse(w, r, v.Errors)
 		return
 	}
 
