c7f75a080ad03e8b0c99199ffc5be97647ada842
zrotrasukha <zrotrasukha@gmail.com>
2026-04-08 05:19:30 +0530

    Fix typo and Add UNIQUE constraint in email field

--- migrations/000004_create_users_table.up.sql
+++ migrations/000004_create_users_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
-    id bigserial PAIMARY KEY,
+    id bigserial PRIMARY KEY,
     created_at timestamptz NOT NULL DEFAULT now(),
     name text NOT NULL,
-    email citext NOT NULL,
+    email citext UNIQUE NOT NULL,
     password_has bytea NOT NULL,
     activated bool NOT NULL,
     version integer NOT NULL DEFAULT 1
