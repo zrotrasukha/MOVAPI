6a3a89010721ece11cf4907bfc61afbd45d5cc9a
zrotrasukha <zrotrasukha@gmail.com>
2026-04-08 06:03:14 +0530

    Add validation methods

--- internal/data/users.go
+++ internal/data/users.go
@@ -4,10 +4,11 @@ import (
 	"errors"
 	"time"
 
+	"github.com/zrotrasukha/MOVAPI/internal/validator"
 	"golang.org/x/crypto/bcrypt"
 )
 
-type Users struct {
+type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	Name      string    `json:"name"`
@@ -46,4 +47,32 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func validateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func validatePassword(v *validator.Validator, password string) {
+	v.Check(password != "", "email", "must be provided")
+	v.Check(len(password) < 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) >= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be longer than 500 bytes long")
+
+	validateEmail(v, user.Email)
+	if user.Password.plaintext != nil {
+		validatePassword(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		// A panic error will occur because, the has can
+		// only be missing because of developers fault; logic error
+
+		panic("missing password hash for user")
+	}
 }
