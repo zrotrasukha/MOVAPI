2a79f23b1f2bb784579f845b9c40d576744e5fe2
zrotrasukha <zrotrasukha@gmail.com>
2026-04-08 13:34:25 +0530

    Fix typo

--- migrations/000004_create_users_table.up.sql
+++ migrations/000004_create_users_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     created_at timestamptz NOT NULL DEFAULT now(),
     name text NOT NULL,
     email citext UNIQUE NOT NULL,
-    password_has bytea NOT NULL,
+    password_hash bytea NOT NULL,
     activated bool NOT NULL,
     version integer NOT NULL DEFAULT 1
 );
