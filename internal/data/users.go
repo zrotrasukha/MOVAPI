2c9f38cc7d7a5bdce87c45278195ac745797ff08
zrotrasukha <zrotrasukha@gmail.com>
2026-04-10 00:55:14 +0530

    Add Insert(), GetByEmail(), Update() and fix bugs (details given)
    Bugs fixed:
    1. Logical error in password validation for minimum and maximum
    characters allowed for password
    2. Convert the normal User parameter to a pointer (*User), because it
    needs to be mutated

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,11 +1,18 @@
 package data
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"time"
 
+	"github.com/lib/pq"
 	"github.com/zrotrasukha/MOVAPI/internal/validator"
 	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrDuplicateEmail = errors.New("duplicate email")
 )
 
 type User struct {
@@ -49,6 +56,96 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	return true, nil
 }
 
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m UserModel) Insert(user *User) error {
+	query := `INSERT INTO users (name, email, password_hash, activated)
+						VALUES($1, $2, $3, $4)
+						RETURNING id, created_at, version`
+
+	args := []any{user.Name, user.Email, user.Password.hash, user.Activated}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt, &user.Version)
+	if err != nil {
+		var pqerr *pq.Error
+		if errors.As(err, &pqerr) && pqerr.Code == "23505" && pqerr.Constraint == "users_email_key" {
+			return ErrDuplicateEmail
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (m *UserModel) GetByEmail(email string) (*User, error) {
+	query := `SELECT id, created_at, name, email, password_hash, activated version
+						FROM users
+						WHERE email = $1`
+
+	var user User
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &user, nil
+}
+
+func (m UserModel) Update(user *User) error {
+	query := `UPDATE users
+	SET name = $1, email = $2, password_hash = $3, activated = $4, version= version + 1
+	WHERE id = $5 AND version = $6
+	RETURNING version`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	args := []any{
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.ID,
+		&user.Version,
+	}
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
+
+}
 func validateEmail(v *validator.Validator, email string) {
 	v.Check(email != "", "email", "must be provided")
 	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
@@ -56,11 +153,11 @@ func validateEmail(v *validator.Validator, email string) {
 
 func validatePassword(v *validator.Validator, password string) {
 	v.Check(password != "", "email", "must be provided")
-	v.Check(len(password) < 8, "password", "must be at least 8 bytes long")
-	v.Check(len(password) >= 72, "password", "must not be more than 72 bytes long")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
 }
 
-func ValidateUser(v *validator.Validator, user User) {
+func ValidateUser(v *validator.Validator, user *User) {
 	v.Check(user.Name != "", "name", "must be provided")
 	v.Check(len(user.Name) <= 500, "name", "must not be longer than 500 bytes long")
 
