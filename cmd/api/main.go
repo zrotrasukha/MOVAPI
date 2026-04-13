7989e1549f917b08de1329d855d851df5439fdbd
zrotrasukha <zrotrasukha@gmail.com>
2026-04-13 23:07:49 +0530

    Remove uneccessary comment

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -75,7 +75,6 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limited maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
-	//TODO: add smtp flags
 	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
 	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "8b704e22f6cf31", "SMTP username")
