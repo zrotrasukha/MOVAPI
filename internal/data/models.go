9701c13dcefd6c3fe59ac0d9e8a0ef65b335609a
zrotrasukha <zrotrasukha@gmail.com>
2026-04-14 17:48:29 +0530

    Introduce TokenModel in Models struct

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,11 +13,13 @@ var (
 type Models struct {
 	Movies MovieModel
 	Users  UserModel
+	Token  TokenModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
 		Users:  UserModel{DB: db},
+		Token:  TokenModel{DB: db},
 	}
 }
